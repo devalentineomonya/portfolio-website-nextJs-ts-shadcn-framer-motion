e425d83ac406e5422af832bfb8c89f2c07f592e7
Valentine Omonya <valomosh254@gmail.com>
2026-02-23 20:29:45 +0300

    Updated particles provider component

--- providers/particles-provider.tsx
+++ providers/particles-provider.tsx
@@ -1,18 +1,33 @@
 "use client";
 import { Particles } from "@/components/ui/particles";
+import { useScreenSize } from "@/hooks/use-screen-size";
 import { useTheme } from "next-themes";
 import { useMemo } from "react";
 
 export const ParticlesProvider = () => {
   const { theme } = useTheme();
+  const isMobile = useScreenSize(640);
+  const isTablet = useScreenSize(768);
+  const isSmallDesktop = useScreenSize(1024);
+  const isMediumDesktop = useScreenSize(1440);
+
+  const quantity = useMemo(() => {
+    if (isMobile) return 50;
+    if (isTablet) return 100;
+    if (isSmallDesktop) return 150;
+    if (isMediumDesktop) return 200;
+    return 300;
+  }, [isMobile, isTablet, isSmallDesktop, isMediumDesktop]);
+
   const color = useMemo(
     () => (theme === "dark" ? "#ffffff" : "#131317"),
     [theme],
   );
+
   return (
     <Particles
       className="fixed inset-0 pointer-events-none"
-      quantity={300}
+      quantity={quantity}
       ease={80}
       staticity={15}
       color={color}
